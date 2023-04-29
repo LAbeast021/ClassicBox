7cea74b2eb8497e5057c097a849f3319ffce016a
LAbeast021 <kiarashhesampour@gmail.com>
2023-04-29 12:30:07 -0700

    original post schema is up

---
+++ models/post.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema ({
+    comment:String,
+    visitorId: String,
+    visitorAvatar: String,
+    visitorName: String
+},{
+    timestamps:true
+})
+
+var postSchema = new Schema({
+    image: String,
+    ratin: Number,
+    ratinArray: [ratingSchema],
+    likes:[],
+    caption: String,
+    userId: String,
+    username: String,
+    userAvatar: String,
+    comments:[commentSchema]
+
+},{
+    timestamps:true
+})
+
+
+module.exports = mongoose.model('Post',postSchema);
